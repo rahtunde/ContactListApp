6944fdb5bc5da9fed855050a247b422daf27d2b9
Tunde Subair <rahtunde@gmail.com>
2023-06-14 19:36:36 +0100

    ch(app): Add Apllication Files

---
+++ contacts1/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Contact
+
+
+class ContactSerializers(ModelSerializer):
+
+    class Meta:
+        model = Contact
+
+        fields = ['country_code','id', 'first_name', 'last_name', 'phone_number', 'contact_picture', 'is_favourite']
